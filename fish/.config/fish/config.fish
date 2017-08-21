19a761ae76fbfe938a4b4684eaba9a5456e64269
Emil Lauridsen <mine809@gmail.com>
2017-08-21 13:03:42 +0200

    ccache

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,3 +3,5 @@ set -x PATH $PATH $GOPATH/bin $HOME/bin $HOME/.cargo/bin
 set -x PATH $PATH $HOME/dev/wine-dirs/build32
 set -x XKB_DEFAULT_LAYOUT dk
 set -x XKB_DEFAULT_VARIANT nodeadkeys
+set -x CCACHE_DIR $HOME/.ccache
+set -x PATH /usr/lib/ccache/bin $PATH
