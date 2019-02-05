6312281588ea0ce4d55afd7f5038164a03077ecf
Emil Lauridsen <mine809@gmail.com>
2019-02-05 11:16:32 +0100

    Auto run a single ssh-agent and add key on first ssh

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,29 +1,30 @@
 set -x GOPATH $HOME/sync/dev/go
-
 set -x CCACHE_DIR $HOME/.ccache
-
 set -x XKB_DEFAULT_LAYOUT dk
 set -x XKB_DEFAULT_VARIANT nodeadkeys
 
-function append_path
-  if test -d $argv
-    set -x PATH $PATH $argv
-  end
-end
-
-function prepend_path
-  if test -d $argv
-    set -x PATH $argv $PATH
-  end
-end
-
-append_path $GOPATH/bin
-append_path $HOME/bin
-append_path $HOME/.cargo/bin
-append_path $HOME/dev/wine-dirs/build32
-
-prepend_path /usr/lib/ccache/bin
-
-set -x FS_LEX_YACC_LIBS /home/kiljacken/lib/FsLexYacc.Runtime.7.0.6/lib/portable-net45+netcore45+wpa81+wp8+MonoAndroid10+MonoTouch10/
+set -x PATH $PATH $GOPATH/bin $HOME/bin $HOME/.cargo/bin
 
 alias wine32 "env WINEPREFIX=$HOME/.wine32 WINEARCH=win32 wine"
+
+# Check if an existing ssh-agent exists and re-use it
+set GOT_AGENT 0
+for FILE in /tmp/ssh-*/agent.*;
+  set SOCK_PID (string split "." $FILE)[2]
+  set PID (ps -fu$LOGNAME|awk '/ssh-agent/ && ( $2=='$SOCK_PID' || $3=='$SOCK_PID' || $2=='$SOCK_PID' +1 ) {print $2}')
+  set SOCK_FILE $FILE
+
+  set -x SSH_AUTH_SOCK $SOCK_FILE
+  set -x SSH_AGENT_PID $PID
+
+  ssh-add -l > /dev/null
+  if [ $status != 2 ]
+    set GOT_AGENT 1
+    echo "Found existing agent pid $PID"
+    break
+  end
+end
+
+if [ $GOT_AGENT = 0 ]
+  eval (ssh-agent -c)
+end
