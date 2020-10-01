f069e6b8f3157436e772d411c499a0b7614cd062
Emil Lauridsen <mine809@gmail.com>
2020-10-01 17:17:11 +0200

    audio: Tweak jack settings to include microphone

--- bin/bin/start_jack.sh
+++ bin/bin/start_jack.sh
@@ -2,8 +2,10 @@
 jack_control start
 jack_control ds alsa
 jack_control dps device hw:MKII
+jack_control dps capture hw:Microphone
+jack_control dps playback hw:MKII
 jack_control dps rate 44100
-jack_control dps nperiods 3 # 2 for MoBo/PCI, 3 for USB
+jack_control dps nperiods 3
 jack_control dps period 512
-sleep 10
+sleep 2
 qjackctl -platform wayland &
